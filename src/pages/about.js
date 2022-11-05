651c5fee4999accd5e05d33d3a1fe022081fa8e0
Evripidis Katsianos <euripidiskatsianos@gmail.com>
2022-11-05 23:47:43 +0200

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from './../components/Layout';
+
+export default function About()  {
+  return (
+    <Layout>
+      <div>
+          <h1>About Page</h1>
+          <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+          <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+          <p>is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</p>
+      </div>
+    </Layout>
+  )
+}
