c7aa7e03c84adc4a8410a9ab4c3bc4aba487e750
Evripidis Katsianos <euripidiskatsianos@gmail.com>
2022-11-06 02:01:20 +0200

    second commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,28 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    'gatsby-transformer-remark',
+
+/*     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `notes`,
+        path: `${__dirname}/src/notes/`,
+      },
+    }, */
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: 'Boat Site',
+    description: 'web dev portfolio',
+    copyright: 'This website is copyright 2021 Web Warrior',
+    contact: 'me@thewebwarrior.com'
+  },
 }
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,14 +1,50 @@
 import React from 'react'
 import Layout from './../../components/Layout';
 import * as styles from '../../styles/projects.module.css'
+import { graphql, Link } from 'gatsby';
 
-export default function Projects() {
+export default function Projects({data}) {
+  console.log(data)
+  const projects = data.projects.nodes
+  const contact = data.contact.siteMetadata.contact
+
   return (
     <Layout>
       <div className={styles.portfolio}>
         <h2>Portofolio</h2>
         <h3>Projects & Websites I've Created</h3>
+        <div className={styles.projects}>{projects.map(project =>(
+          <Link to={"/projects/" + project.frontmatter.slug} key={project.id}>
+            <div>
+              <h3>{project.frontmatter.title}</h3>
+              <p>{project.frontmatter.stack}</p>
+            </div>
+          </Link>
+        ))}</div>
+        <p>Like what you see? Email me at {contact} for a quote!</p>
       </div>
   </Layout>
   )
 }
+
+// export page query
+export const query = graphql`
+query MyQuery {
+  projects: allMarkdownRemark(sort: {order: ASC, fields: frontmatter___date}) {
+    nodes {
+      frontmatter {
+        title
+        stack
+        slug
+        date
+      }
+      id
+    }
+  }
+  contact: site {
+    siteMetadata {
+      contact
+    }
+  }
+}
+`
